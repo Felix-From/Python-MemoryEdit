23c3e0e01e98a573f0609d9c6e3cd85185c86202
Coffee <DTWOW13@web.de>
2023-12-10 06:34:30 +0100

    Freeze+LookUp für MemWork + Commented
    Pointer an Variabel Freezen
    (create,trigger(schalter),remove)
    
    Pointer beobachten und triggern wenn änderung
    (create,trigger,remove)

--- MemWork.py
+++ MemWork.py
@@ -1,5 +1,13 @@
-from pymem import *
+import pymem
 from pymem.process import module_from_name
+import threading as Thread
+
+def __dprint(stuff_to_print,loop = None):
+    global debug
+    if debug:
+        if (debug_freezeloop and loop == 1) or (debug_lookUploop and loop == 2) or loop is None:
+            print(stuff_to_print)
+    return stuff_to_print
 
 #Process und Module Stuff
 
@@ -10,23 +18,226 @@ def getProcessAndModuleFromName(ProcessName,moduleName): #Returns Process , modu
 
 # Pointer Stuff
 
-def __getPtrAddr(process, address, offsets, debug=False):
+def __getPtrAddr(process, address, offsets):
+    global debug
     try:
         addr = process.read_longlong(address)
-        if debug:
-            print(f"Initial Address: 0x{addr}")
+        __dprint(f"Initial Address: 0x{addr}")
         for offset in offsets:
             if offset != offsets[-1]:
                 addr = process.read_longlong(addr + offset)
-                if debug:
-                    print(f"Offset 0x{offset}: 0x{addr}")
+                __dprint(f"Offset 0x{offset}: 0x{addr}")
         addr = addr + offsets[-1]
-        if debug:
-            print(f"Final Address: 0x{addr}")
+        __dprint(f"Final Address: 0x{addr}")
         return addr
     except pymem.exception.MemoryReadError as e:
         print(f"@@@@@@@@@@ Error reading memory at address: {e} @@@@@@@@@@@@")
         return None
 
-def createPointerAddr(process,module_base,StaticOffset,Offsets,debug = False):
-    return __getPtrAddr(process,module_base+StaticOffset,Offsets,debug)
+def createPointerAddr(process,module_base,StaticOffset,Offsets):
+    return __getPtrAddr(process,module_base+StaticOffset,Offsets)
+
+#----------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------
+#-----------Pointer Freezer
+#----------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------
+#   pointersToFreeze[i]             ArrayErklärung
+# i=0   Process
+#   1   Address 
+#   2   Value - Freezewert
+#   3   Valuetype - als str den typ z.B. "int"
+#   4   state - active True/False
+#   5   lastRead
+
+
+def createPointerFreezer(process, address, value, valuetype, state=False):
+    exist = any(pointer[1] == address for pointer in pointersToFreeze)
+    if not exist:
+        pointersToFreeze.append([process, address, value, valuetype, state, None])
+                                                                            #None = lastRead
+    return
+
+def triggerPointerFreezer(address,state,value=None,valuetype=None):
+    #Trigger True/False je nach state, aber man kann auch value und valuetype anpassen lassen.
+    __dprint("triggerPointerFreezer | "+str(address)+"|"+str(state))
+    for pointer in pointersToFreeze:
+        if pointer[1] == address: # Wenn es einen eintrag findet mit der Adresse
+            __dprint("triggerFreeze Pointer Found.")
+            if value is not None: # Wenn gesetzt dann setzt er die die Value auf Value
+                pointer[2] = value
+            if valuetype is not None: #gleiche wie oben
+                pointer[3] = valuetype
+            pointer[4] = state #state True/False ob es gefreezed sein soll oder nicht
+            pointer[5] = None  #lastRead
+            break
+    return
+
+def removePointerFreezer(address):
+    callback = False
+    for pointer in pointersToFreeze:
+        if pointer[1] == address:
+            __dprint("Removed PointerFreezer "+hex(address))
+            pointersToFreeze.remove(pointer)
+            break
+    return callback
+
+def __freezeLoop():
+    global pointersToFreeze
+    while True:
+        if pointersToFreeze != []:
+            for pointer in pointersToFreeze:
+                if pointer[4]: #Is active?
+                    try:
+                        if pointer[0].read_int(pointer[1]) != pointer[5]:
+                            __dprint("Freezeloop Pointer Write für "+hex(pointer[1])+" Value:"+str(pointer[2]),1)
+                            match pointer[3]:
+                                case "int":
+                                    pointer[0].write_int(pointer[1],pointer[2])
+                                    pointer[5] = pointer[2]
+                                    __dprint("Freezeloop write int",1)
+                                case "float":
+                                    pointer[0].write_float(pointer[1],pointer[2])
+                                    pointer[5] = pointer[2]
+                                    __dprint("Freezelóop write float",1)
+                                case "string":
+                                    pointer[0].write_string(pointer[1],pointer[2])
+                                    pointer[5] = pointer[2]
+                                    __dprint("Freezeloop write string",1)
+                                case "bytes":
+                                    pointer[0].write_bytes(pointer[1],pointer[2],4)
+                                    pointer[5] = pointer[2]
+                                    __dprint("Freezeloop write bytes",1)
+                    except pymem.exception.MemoryReadError as e:
+                        print(f"@@@@@@@@@@ [FreezerPointer] Error reading memory at address: {e} @@@@@@@@@@@@")
+                        return None
+    return
+
+#----------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------
+#-----------LookUp - Change -
+#----------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------
+#   pointersToLookUp[i]             ArrayErklärung
+# i=0   process
+#   1   address
+#   2   type
+#   3   triggerName (Print bei Trigger)
+#   4   triggerFunction (pointer)
+#   5   triggerValue (id oder so für selbst benutzung.)
+#   6   lastRead - Letzer Wert seit veränderung.
+#   7   triggerState - active True/False
+
+def createLookUpAddress(process,address,type,triggerName,triggerFunction,triggerValue, lastRead=-1,triggerState = False):
+    exist = any(pointer[1] == address for pointer in pointersToLookUp)
+    if not exist:
+        pointersToLookUp.append([process,address,type,triggerName,triggerFunction,triggerValue, lastRead,triggerState])
+    return
+
+def triggerLookUpAddress(address,state,resetLastRead = False):
+    #Trigger True/False je nach state.
+    __dprint("triggerLookUpAdress | "+str(address)+"|"+str(state))
+    for pointer in pointersToLookUp:
+        if pointer[1] == address: # Wenn es einen eintrag findet mit der Adresse
+            __dprint("triggerLookUpAddress Pointer Found.")
+            pointer[7] = state
+            if resetLastRead:
+                pointer[6] = 0
+            break
+    return
+
+def removeLookUpAddress(address):
+    for pointer in pointersToLookUp:
+        if pointer[1] == address:
+            __dprint("Removed lookUpAddress "+hex(address))
+            pointersToLookUp.remove(pointer)
+            break
+
+def __onTriggerLookUp(pointer):
+    __dprint("onTriggerLookUp! Found! -> "+str(pointer[3]))
+    match pointer[2]:
+        case "int":
+            pointer[4](pointer[5],pointer[0].read_int(pointer[1]))
+        case "float":
+            pointer[4](pointer[5],pointer[0].read_float(pointer[1]))
+        case "bytes":
+            pointer[4](pointer[5],int.from_bytes(pointer[0].read_bytes(pointer[1],4)))
+        case "string":
+            pointer[4](pointer[5],pointer[0].read_string(pointer[1]))
+    return
+
+def __lookUpLoop():
+    while True:
+        for pointer in pointersToLookUp:
+            if pointer[7]:
+                try:
+                    match pointer[2]:
+                        case "int":
+                            if pointer[0].read_int(pointer[1]) != pointer[6]:
+                                debug_print_Text = "LookUpPool "+hex(pointer[1])+" read new value! Old: "+str(pointer[6])+" | New:"
+                                pointer[6] = pointer[0].read_int(pointer[1])
+                                __dprint(debug_print_Text+str(pointer[6]),2)
+                                __onTriggerLookUp(pointer)
+                        case "float":
+                            if pointer[0].read_float(pointer[1]) != pointer[6]:
+                                debug_print_Text = "LookUpPool "+hex(pointer[1])+" read new value! Old: "+str(pointer[6])+" | New:"
+                                pointer[6] = pointer[0].read_float(pointer[1])
+                                __dprint(debug_print_Text+str(pointer[6]),2)
+                                __onTriggerLookUp(pointer)
+                        case "bytes":
+                            if int.from_bytes(pointer[0].read_bytes(pointer[1],4)) != pointer[6]:
+                                debug_print_Text = "LookUpPool "+hex(pointer[1])+" read new value! Old: "+str(pointer[6])+" | New:"
+                                pointer[6] = int.from_bytes(pointer[0].read_bytes(pointer[1],4))
+                                __dprint(debug_print_Text+str(pointer[6]),2)
+                                __onTriggerLookUp(pointer)
+                        case "string":
+                            if pointer[0].read_string(pointer[1]) != pointer[6]:
+                                debug_print_Text = "LookUpPool "+hex(pointer[1])+" read new value! Old: "+str(pointer[6])+" | New:"
+                                pointer[6] = pointer[0].read_string(pointer[1])
+                                __dprint(debug_print_Text+str(pointer[6]),2)
+                                __onTriggerLookUp(pointer)
+                except pymem.exception.MemoryReadError as e:
+                    print(f"@@@@@@@@@@ [FreezerPointer] Error reading memory at address: {e} @@@@@@@@@@@@")
+                    return None
+                pass
+        pass
+
+#----------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------
+#-----------Multithreading
+#----------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------
+
+def __startFreezeLoop():
+    freezeThread = Thread.Thread(target = __freezeLoop)
+    freezeThread.daemon = True #Damit die Threads mit zu gehen wenn man das Programm schließt
+    freezeThread.start()
+    return freezeThread
+
+def __startLookUpLoop():
+    loopUpThread = Thread.Thread(target = __lookUpLoop)
+    loopUpThread.daemon = True # Ohne das würden die Threads einfach offen bleiben und ram fressen.
+    loopUpThread.start()
+    return loopUpThread
+
+#----------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------
+#-----------Debug Zone.
+#----------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------
+
+debug = True
+debug_freezeloop = False
+debug_lookUploop = True
+
+
+#----------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------
+#-----------Main init.
+#----------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------
+
+pointersToFreeze = []
+freezeThread = __startFreezeLoop()
+pointersToLookUp = []
+loopUpThread = __startLookUpLoop()
--- test.py
+++ test.py
@@ -1,13 +1,72 @@
 import pymem as pm
 import MemWork
 
+#-----------------------------------------------------------
+# MemWork Basic Pointer
 mem , module_base = MemWork.getProcessAndModuleFromName("Stardew Valley.exe","coreclr.dll")
 
-time_staticOffset = 0x004AD958
-time_offsets = [0x0, 0x80, 0x28, 0x318, 0x628]
-time_adress = MemWork.createPointerAddr(mem,module_base,time_staticOffset,time_offsets)
+#time_staticOffset = 0x004AD958
+#time_offsets = [0x0, 0x80, 0x28, 0x318, 0x628]
+#time_adress = MemWork.createPointerAddr(mem,module_base,time_staticOffset,time_offsets)
+    # oder als one liner
+#time_adress = MemWork.createPointerAddr(mem,module_base,0x004AD958,[0x0, 0x80, 0x28, 0x318, 0x628])
 
-time_adress = MemWork.createPointerAddr(mem,module_base,0x004AD958,[0x0, 0x80, 0x28, 0x318, 0x628])
+#-----------------------------------------------------------
+# Memwork Freeze Pointer
 
+    # Stamina_adress wie normal erstellen
+stamina_address = MemWork.createPointerAddr(mem,module_base,0x004A43A8,[0x1E8,0x40,0xA8,0xB8C,0x10,0x370,0x9DC])
+    # Zum erstellen des PinterFreezers ein mal ausführen mit den Daten
+MemWork.createPointerFreezer(mem,stamina_address,260.0,"float")
+    # Zum ändern des Triggerzustandes oder der Value/Valuetype  Value/Valuetype kann leer gelassen werden wenn nichts verändert wird.
+MemWork.triggerPointerFreezer(stamina_address,True)
+#MemWork.triggerPointerFreezer(stamina_address,False,10,"int")
 
-mem.write_int(time_adress,930)
+#-----------------------------------------------------------
+
+#Memwork LookUp Pointer
+
+    #Funktion erstellen die bei Veränderung getriggert werden soll
+    #Wichtig 2 Parameter, hier: traiggerValue, itemValue (name egal)
+    #Wenn der Trigger hier ankommt steht in triggerValue eine selbst ausgesuchte 
+    #triggerValue(falls man 1 callback für mehrere pointer benutzt zum zuordnen)
+    #Und in itemValue steht der veränderte wert.
+def __callbackFunction(triggerValue,itemValue):
+    print("Test bestanden | "+ str(triggerValue)+ " | " + str(itemValue))
+    return
+
+    # Stamina_adress wie normal erstellen
+stamina_address = MemWork.createPointerAddr(mem,module_base,0x004A43A8,[0x1E8,0x40,0xA8,0xB8C,0x10,0x370,0x9DC])
+
+    #createLookUpAddress process,address,type(als str),"AnzeigeName",Callback Function, triggerValue , startwert (ohne angabe = -1)
+_type = "int" # muss als Str sein.
+AnzeigeName = "Angeln" #Wird beim OnTriggerLookUP in Console geschrieben
+#__callbackFunction die Funktion die es ausrufen soll wenn der wert sich ändert.
+triggerValue = 5 #Ein von dir zugewiesener Text der beim callback als erste Variable wieder raus gegeben wird ( zur zuweisung oder so)
+lastRead = 270.0 #Der Wert mit dem er starten soll, wenn weg gelassen wird ist er -1
+#Aber Achtung, bei -1 wird er zu 100% min. 1 mal triggern nach erstellung.(Wenn er Aktiv getriggert wird das erste mal)
+MemWork.createLookUpAddress(mem,0x1F7022083A4,_type,AnzeigeName,__callbackFunction,triggerValue,lastRead)
+#vergleicht den lastRead mit aktuell und wenn es nicht so ist triggert er deine Funktion
+#und speichert den neuen wert in lastRead damit er nicht 2x los geht.
+MemWork.triggerLookUpAddress(0x1F7022083A4,True)
+
+def debug_func():
+    MemWork.triggerPointerFreezer(stamina_address,False)
+    MemWork.triggerLookUpAddress(0x1F7022083A4,False)
+
+    print(str(MemWork.pointersToFreeze[0]))
+    print(str(MemWork.pointersToLookUp[0]))
+
+    MemWork.removePointerFreezer(stamina_address)
+    MemWork.removeLookUpAddress(0x1F7022083A4)
+
+    print(str(MemWork.pointersToFreeze))
+    print(str(MemWork.pointersToLookUp))
+    return
+
+import keyboard
+keyboard.on_release_key("F1",lambda _:debug_func())
+
+#Damit das Programm laufen bleibt.
+while True:
+    pass
